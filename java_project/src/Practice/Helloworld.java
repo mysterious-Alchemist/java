d139ecd82bd361ab28d781115cb8e096f9953fab
mysterious-Alchemist <2chhoon@gmail.com>
2023-01-04 13:02:51 +0900

    java hello world

---
+++ java_project/src/Practice/Helloworld.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		//컨트롤 + f11 = 실행
+		
+		 //전체주석:
+		 /*
+		   
+		 여기는 모두 주석
+		   
+		  */
+		 
+	}
+
+}
